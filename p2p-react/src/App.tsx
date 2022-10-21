0ca456c9e7cbee0de39487562e945d89eb261a20
David DiMaria <info@collectivesessions.com>
2022-10-21 15:09:00 -0600

    Add console logging and improve overall experience and code quality

--- p2p-react/src/App.tsx
+++ p2p-react/src/App.tsx
@@ -40,12 +40,19 @@ type FormInputs = {
 
 type FormSubmitType = "dial" | "message";
 
+type Peers = {
+  id: string;
+  multiAddr: string;
+  stream: Stream;
+};
+
 function App() {
   // const [errMsg, setErrMsg] = useState("");
   // const [resultMsg, setResultMsg] = useState("");
+  const [logger, setLogger] = useState<string[]>([]);
   const [_goMultiAddr, setGoMultiAddr] = useState("");
   const [createMessage, setCreateMessage] = useState("");
-  const [streams, setStreams] = useState<Stream>();
+  const [peers, setPeers] = useState<Map<string, Peers>>(new Map());
   const browserNode = {
     nodes: [{ id: "Browser", image: "./chrome.png" }],
     links: [],
@@ -59,18 +66,28 @@ function App() {
     { label: "Rust", value: "rust" },
   ];
 
+  const truncate = (value: string, size: number = 20): string => {
+    const half = Math.max(size, 1)/2;
+  
+    if (value.length > 20) {
+      return value.substring(0, half) + '...' + value.substring(value.length - half);
+    }
+
+    return value;
+  }
+
   // add a node to the graph
-  const AddNode = async (id: string) => {
+  const AddNode = async (id: string, sdk: string) => {
     await setData(({ nodes, links }) => {
       const data = {
-        nodes: [{ id, image: `./${id}.png` }],
+        nodes: [{ id, image: `./${sdk}.png` }],
         links: [
           { source: id, curvature: 0.8, rotation: 0, target: "Browser" }, // line to the browser
           { source: "Browser", curvature: 0.8, rotation: 0, target: id }, // line from the browser
         ],
       };
 
-      console.log("Add Node: ", data);
+      console.log("Adding node: ", data);
 
       return {
         nodes: [...nodes, ...data.nodes],
@@ -91,7 +108,7 @@ function App() {
         z: distance * Math.cos(angle),
       });
       angle += Math.PI / 300;
-    }, 100);
+    }, 50);
   }, []);
 
   const {
@@ -103,63 +120,95 @@ function App() {
     mode: "onBlur",
   });
 
+  // handle all form submissions
   const onSubmit = async (values: FormInputs) => {
     const type: FormSubmitType = values.goMultiAddr !== "" ? "dial" : "message";
-    console.log("onSubmit type", type);
 
     switch (type) {
       case "dial":
-        dial(values.goMultiAddr, values.dialSdk);
+        let id = await dial(values.goMultiAddr, values.dialSdk);
+        setLogger(logger.concat(`Dialing ${values.dialSdk} peer ${truncate(id)}`));
         reset({ goMultiAddr: "", dialSdk: "" });
         break;
       case "message":
+        setLogger(logger.concat(`Sending message "${values.message}" to ${truncate(values.peer)} peer`));
         await message(values.message, values.peer);
         reset({ message: "", peer: "" });
         break;
     }
   };
 
+  // handle incoming and outgoing streams
   useEffect(() => {
     if (createMessage) console.log("createMessage", createMessage);
-    if (streams) console.log("streams", streams);
-    if (createMessage && streams) {
-      // log response to console
-      pipe(
-        streams.source,
-        (source) => map(source, (buf) => uint8arrayToString(buf.subarray())),
-        // Sink function
-        async (source) => {
-          // For each chunk of data
-          for await (const msg of source) {
-            // Output the data as a utf8 string
-            console.log("> " + msg.toString().replace("\n", ""));
+    if (peers) console.log("streams", peers);
+
+    peers.forEach((peer, id) => {
+      if (createMessage) {
+        console.log("peer.stream.source", peer.stream.source);
+        // log response to console
+        pipe(
+          peer.stream.source,
+          (source) => map(source, (buf) => {
+            console.log("source", source);
+            console.log("buf", buf);
+            return uint8arrayToString(buf.subarray());
+          }),
+          // Sink function
+          async (source) => {
+            // For each chunk of data
+            for await (const msg of source) {
+              // Output the data as a utf8 string     
+              const parsed = msg.toString().replace("\n", "");  
+              setLogger(logger.concat(`Received message "${parsed}" from peer ${truncate(peer.stream.id)}`));
+              console.log("> " + parsed);
+            }
           }
-        }
-      );
+        );
+  
+        // send message to the listener
+        pipe([uint8arrayFromString(createMessage)], peer.stream.sink);
+      }
+    })
+  }, [createMessage, peers]);
 
-      // send message to the listener
-      pipe([uint8arrayFromString(createMessage)], streams.sink);
-    }
-  }, [createMessage, streams]);
-
-  const dial = async (goMultiAddr: string, sdk: string) => {
+  // connect to a peer
+  const dial = async (goMultiAddr: string, sdk: string): Promise<string> => {
     await setGoMultiAddr(goMultiAddr);
-    await AddNode(sdk);
-    stream = await dialWebRtc(goMultiAddr);
+    const stream = await dialWebRtc(goMultiAddr);
+    await AddNode(stream.id, sdk);
 
-    setStreams(stream);
+    const peer: any = {
+      id: stream.id,
+      multiAddr: goMultiAddr,
+      stream,
+    }
+
+    setPeers(peers.set(stream.id, peer));
+
+    return stream.id;
   };
 
+  // initiate sending a message to a peer
   const message = async (msg: string, peer: string) => {
+    // show data movement on the graph
+    // send from Browser to Peer first, the Peer to Browser
     data.links
       .filter(
         (link) =>
           (link.target as any).id === peer || (link.source as any).id === peer
       )
-      .forEach((link) => (fgRef.current as any).emitParticle(link));
+      .forEach((link) => {
+        const delay =  (link.target as any).id === peer ? 0 : 300;
+        setTimeout(() => (fgRef.current as any).emitParticle(link), delay);
+      });
 
     setCreateMessage(msg + "\n");
   };
+
+  // setInterval(() => {
+  //   setLogger(logger.concat(`sdfafsdafdsafsdfasdafsd`));
+  // }, 500);
 
   return (
     <div>
@@ -186,7 +235,7 @@ function App() {
               <GridItem w="100%">
                 <Input
                   {...register("goMultiAddr", { required: false })}
-                  placeholder="Go Client MultiAddress"
+                  placeholder="Peer MultiAddress"
                   size="lg"
                 />
               </GridItem>
@@ -209,6 +258,7 @@ function App() {
                   leftIcon={<MdCall />}
                   isLoading={isSubmitting}
                   onClick={handleSubmit(onSubmit)}
+                  width={100}
                 >
                   Dial
                 </Button>
@@ -228,9 +278,9 @@ function App() {
                   placeholder="Peer"
                   size="lg"
                 >
-                  {sdks.map((option, index) => (
-                    <option key={index} value={option.value}>
-                      {option.label}
+                  {Array.from(peers.values()).map((key, index) => (
+                    <option key={index} value={key.id}>
+                      {truncate(key.id)}
                     </option>
                   ))}
                 </Select>
@@ -241,6 +291,7 @@ function App() {
                   leftIcon={<MdScheduleSend />}
                   isLoading={isSubmitting}
                   onClick={handleSubmit(onSubmit)}
+                  width={100}
                 >
                   Send
                 </Button>
@@ -250,49 +301,64 @@ function App() {
 
           {/* <div style={{marginTop: "20px"}}>Result: {resultMsg}</div> */}
 
-          <ForceGraph3D
-            ref={fgRef}
-            // enableNodeDrag={false}
-            // enableNavigationControls={false}
-            showNavInfo={false}
-            graphData={data}
-            width={800}
-            height={500}
-            // onEngineStop={() => (fgRef.current as any).zoomToFit(1800)}
-            backgroundColor={"#fff"}
-            nodeColor={() => "#000"}
-            // nodeRelSize={4}
-            linkColor={() => "#000"}
-            linkOpacity={1}
-            linkWidth={0.1}
-            linkCurvature="curvature"
-            linkCurveRotation="rotation"
-            // linkDirectionalParticles={2}
-            linkDirectionalParticleWidth={1}
-            linkDirectionalParticleColor={() => "red"}
-            linkDirectionalParticleSpeed={0.1}
-            onLinkClick={(link) => console.log("link", link)}
-            nodeThreeObject={(node: any) => {
-              // text sprite
-              const spriteText = new SpriteText(node.id);
-              spriteText.color = node.color;
-              spriteText.textHeight = 3;
 
-              // image sprite
-              var map = new TextureLoader().load(node.image);
-              map.minFilter = LinearFilter;
-              const material = new SpriteMaterial({ map: map });
-              const spriteImg = new Sprite(material);
-              spriteImg.scale.set(9, 9, 1);
+          <Grid templateColumns="repeat(2, 1fr)" gap={0}>
+            <GridItem w="100%">
+              <ForceGraph3D
+                ref={fgRef}
+                // enableNodeDrag={false}
+                // enableNavigationControls={false}
+                showNavInfo={false}
+                graphData={data}
+                width={600}
+                height={400}
+                // onEngineStop={() => (fgRef.current as any).zoomToFit(1800)}
+                backgroundColor={"#fff"}
+                nodeColor={() => "#000"}
+                // nodeRelSize={4}
+                linkColor={() => "#000"}
+                linkOpacity={1}
+                linkWidth={0.1}
+                linkCurvature="curvature"
+                linkCurveRotation="rotation"
+                // linkDirectionalParticles={2}
+                linkDirectionalParticleWidth={1}
+                linkDirectionalParticleColor={() => "red"}
+                linkDirectionalParticleSpeed={0.1}
+                onLinkClick={(link) => console.log("link", link)}
+                nodeThreeObject={(node: any) => {
+                  // text sprite
+                  const spriteText = new SpriteText(node.id);
+                  spriteText.color = node.color;
+                  spriteText.textHeight = 3;
 
-              // combine the sprites as a group
-              var group = new Group();
-              // group.add( spriteText );
-              group.add(spriteImg);
+                  // image sprite
+                  var map = new TextureLoader().load(node.image);
+                  map.minFilter = LinearFilter;
+                  const material = new SpriteMaterial({ map: map });
+                  const spriteImg = new Sprite(material);
+                  spriteImg.scale.set(9, 9, 1);
 
-              return group;
-            }}
-          />
+                  // combine the sprites as a group
+                  var group = new Group();
+                  // group.add( spriteText );
+                  group.add(spriteImg);
+
+                  return group;
+                }}
+              />
+
+            </GridItem>
+            <GridItem w="100%">
+                <div style={{ padding: "10px", width: "100%", height: "300px", background: "black", marginTop: "40px", overflowY: "scroll", color: "white"}}>
+                  {logger.map((log, index) => (
+                    <div>{log}</div>
+                  ))}
+                </div>
+            </GridItem>
+          </Grid>
+
+
         </Box>
       </Container>
     </div>
